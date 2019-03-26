1754922f0626650ce14d38c4b2ad14e650c1b45e
Ge Guo <alex@Ges-MacBook-Pro.local>
2019-03-26 16:04:28 +1100

    first update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./App.scss";
 
-class App extends Component {
+export default class App extends React.Component {
+  state = {
+    isOpen: false,
+    activeAcc: "",
+    accordions: [
+      {
+        id: "0",
+        name: "accordion 1",
+        pannel: "pannel 1"
+      },
+      {
+        id: "1",
+        name: "accordion 2",
+        pannel: "pannel 2"
+      },
+      {
+        id: "2",
+        name: "accordion 3",
+        pannel: "pannel 3"
+      }
+    ]
+  };
+
+  handleClick = id => {
+    this.setState(prevState => ({
+      activeAcc: id,
+      isOpen: !prevState.isOpen
+    }));
+  };
+
   render() {
+    const { accordions, isOpen, activeAcc } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {accordions.map(accordion => (
+          <div key={accordion.id} className="accordion">
+            <div onClick={() => this.handleClick(accordion.id)}>
+              {accordion.name}
+            </div>
+            {activeAcc === accordion.id && isOpen ? (
+              <div className="pannel">{accordion.pannel}</div>
+            ) : null}
+          </div>
+        ))}
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  isOpen: PropTypes.bool,
+  activeAcc: PropTypes.string,
+  accordions: PropTypes.array
+};
